0d7bf131e0ca8d4ec73fff52ce46e30cd5373fd5
freeotoole <14006733+freeotoole@users.noreply.github.com>
2021-04-02 09:10:56 +1100

    Moving spreads data to state

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <div id="app" class="container mx-auto">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/settings">Settings</router-link>
-    </div>
-    <router-view/>
+  <div id="app">
+    <header class="bg-gray-700 py-4">
+      <div class="nav container mx-auto text-white">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/settings">Settings</router-link>
+      </div>
+
+    </header>
+    <router-view class="container mx-auto" />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <NewsColumn :options="TopNews" />
-    <NewsColumn :options="FakeNews" />
+    <template v-for="(spread, i) in spreads">
+      <component :is="NewsColumn" :options="spread"  :key="i"/>
+    </template>
   </div>
 </template>
 
@@ -15,46 +16,12 @@ export default {
     NewsColumn,
   },
   data: () => ({
-    TopNews: {
-      title: 'Top News',
-      endpoint: 'everything',
-      sources: 'new-scientist,national-geographic,google-news-au', //
-      pageSize: 9,
-      columns: [
-        {
-          count: 2,
-          classes: 'col-span-2',
-          layout: ['lede', 'horizontal lede'],
-        },
-        {
-          count: 2,
-          classes: 'lg:order-first',
-          layout: 'lede',
-        },
-        {
-          count: 5,
-          layout: 'horizontal',
-        },
-      ],
-    },
-    FakeNews: {
-      title: 'Fake News (Fox)',
-      endpoint: 'everything',
-      sources: 'fox-news', // ,google-news-au
-      pageSize: 9,
-      columns: [
-        {
-          count: 1,
-          classes: 'col-span-2',
-          layout: ['lede', 'horizontal lede'],
-        },
-        {
-          count: 3,
-          classes: 'col-span-2',
-          layout: 'horizontal lede',
-        },
-      ],
-    },
+    // component needs to be registered in data
+    NewsColumn: 'news-column',
+    spreads: [],
   }),
+  mounted() {
+    this.spreads = this.$store.state.spreads;
+  },
 };
 </script>
